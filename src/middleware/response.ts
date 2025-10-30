8440699a777c23846d7294a489bd39f848d66b19
Lee <256260842@qq.com>
2025-10-30 17:02:10 +0800

    service

--- src/middleware/response.ts
+++ src/middleware/response.ts
@@ -1,16 +1,18 @@
 import type { Request, NextFunction } from "express";
-import type { ExtendedResponse } from "../types/extendedResponse";
+import type { CustomResponse } from "../types/customResponse.ts";
 
 export function responseEnhancer(
   req: Request,
-  res: ExtendedResponse,
+  res: CustomResponse,
   next: NextFunction
 ) {
-  res.success = (data?: any) => {
-    res.json({ code: 200, data: data ?? null, msg: "success" });
+  res.success = function (data?: any) {
+    this.json({ code: 200, data: data ?? null, msg: "success" });
   };
-  res.error = (msg?: string, code?: number) => {
-    res.json({ code: code ?? 500, data: null, msg: msg ?? "error" });
+
+  res.error = function (msg?: string, code?: number) {
+    this.json({ code: code ?? 500, data: null, msg: msg ?? "error" });
   };
+
   next();
 }
